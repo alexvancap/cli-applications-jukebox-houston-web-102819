20c5172dc728c4ba1d4eb941eab81ee884c10d51
Alexander Van Cappellen <alexvancap@gmail.com>
2019-10-17 23:43:46 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -28,6 +28,8 @@ def play (songs)
   songs.each_with_index{|song, index|
     if input.to_i == (index + 1)
       puts "Playing #{song}"
+    elsif input == "exit"
+      return goodbye!
     end
   }
 end
